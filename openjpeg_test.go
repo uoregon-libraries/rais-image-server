635a58780df68d9c6d39b67103a2da3bc5e1a9f8
Daniel Krech <eikeon@eikeon.com>
2013-02-21 14:57:57 -0500

    Changed to test more than one jp2 image. Added one of the jp2 images that is resulting in: [ERROR] Cannot handle box of undefined sizes.

--- openjpeg_test.go
+++ openjpeg_test.go
@@ -1,6 +1,8 @@
 package main
 
 import (
+	"crypto/md5"
+	"fmt"
 	"image"
 	"io"
 	"log"
@@ -9,34 +11,40 @@ import (
 	"testing"
 )
 
+var JP2_URLS = []string{"http://chroniclingamerica.loc.gov/lccn/sn83030214/1903-05-14/ed-1/seq-1.jp2", "http://chroniclingamerica.loc.gov/lccn/sn83045433/1913-02-20/ed-1/seq-1.jp2"}
+
 func TestTile(t *testing.T) {
-	path := "/tmp/tile.jp2"
-	_, err := os.Stat(path)
-	if err != nil {
-		f, err := os.Create(path)
+	for _, url := range JP2_URLS {
+		h := md5.New()
+		io.WriteString(h, url)
+		path := fmt.Sprintf("/tmp/brikker-%x.jp2", string(h.Sum(nil)))
+		_, err := os.Stat(path)
 		if err != nil {
-			panic(err)
+			f, err := os.Create(path)
+			if err != nil {
+				panic(err)
+			}
+			resp, err := http.Get(url)
+			if err == nil {
+				log.Println("Status:", resp.Status)
+				io.Copy(f, resp.Body)
+			} else {
+				log.Println("Get error:", err)
+			}
+			f.Close()
 		}
-		resp, err := http.Get("http://chroniclingamerica.loc.gov/lccn/sn83030214/1903-05-14/ed-1/seq-1.jp2")
-		if err == nil {
-			log.Println("Status:", resp.Status)
-			io.Copy(f, resp.Body)
+
+		r := image.Rect(2547, 447, 4298, 1559)
+		width := 681
+		height := 432
+		if err, i := NewImageTile(path, r, width, height); err == nil {
+			log.Println(i.Bounds())
+			if i.Bounds().Max.X < 10 {
+				t.FailNow()
+			}
+
 		} else {
-			log.Println("Get error:", err)
+			log.Fatal("error creating image tile:", err)
 		}
-		f.Close()
-	}
-
-	r := image.Rect(2547, 447, 4298, 1559)
-	width := 681
-	height := 432
-	if err, i := NewImageTile(path, r, width, height); err == nil {
-		log.Println(i.Bounds())
-		if i.Bounds().Max.X < 10 {
-			t.FailNow()
-		}
-
-	} else {
-		log.Fatal("error creating image tile:", err)
 	}
 }
