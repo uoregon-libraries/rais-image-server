cfc6bda2033c8648505ec85700bd4b5df42fda08
Jeremy Echols <jechols@uoregon.edu>
2014-09-16 10:57:28 -0700

    Return higher-quality tiles
    These increase bandwidth compared to chronam's server, but they better
    match the quality chronam produces.
    
    This should really be a config option....

--- cmd/jp2tileserver/main.go
+++ cmd/jp2tileserver/main.go
@@ -56,7 +56,7 @@ func TileHandler(w http.ResponseWriter, req *http.Request) {
 
 	// Serve generated JPG file
 	if err, i := openjpeg.NewImageTile(filepath, r, width, height); err == nil {
-		if err = jpeg.Encode(w, i, nil); err != nil {
+		if err = jpeg.Encode(w, i, &jpeg.Options{Quality: 80}); err != nil {
 			log.Println(err)
 		}
 	} else {
