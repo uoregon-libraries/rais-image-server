b2166e8e66737a6fac34ad6b1e96ca5e7d66c424
Jeremy Echols <jechols@uoregon.edu>
2014-09-18 15:46:07 -0700

    Return from NewImageTile immediately on error

--- openjpeg/openjpeg.go
+++ openjpeg/openjpeg.go
@@ -13,52 +13,48 @@ import (
 	"fmt"
 )
 
-func NewImageTile(jp2 *JP2Image, r image.Rectangle, width, height int) (tile *ImageTile, err error) {
+func NewImageTile(jp2 *JP2Image, r image.Rectangle, width, height int) (*ImageTile, error) {
 	level := desired_progression_level(r, width, height)
 	goLog(6, fmt.Sprintf("desired level: %d", level))
-	//(parameters).cp_reduce = C.OPJ_UINT32(level)
 
-	if err == nil && C.opj_set_decoded_resolution_factor(jp2.codec, C.OPJ_UINT32(level)) == C.OPJ_FALSE {
-		err = errors.New("failed to set decode resolution factor")
+	if C.opj_set_decoded_resolution_factor(jp2.codec, C.OPJ_UINT32(level)) == C.OPJ_FALSE {
+		return nil, errors.New("failed to set decode resolution factor")
 	}
 
 	// Header *must* be read after we determine the decode resolution factor
-	err = jp2.ReadHeader()
-
-	if err == nil {
-		goLog(6, fmt.Sprintf("num comps: %d", jp2.image.numcomps))
-		goLog(6, fmt.Sprintf("x0: %d, x1: %d, y0: %d, y1: %d", jp2.image.x0, jp2.image.x1, jp2.image.y0, jp2.image.y1))
+	if err := jp2.ReadHeader(); err != nil {
+		return nil, err
 	}
 
-	if err == nil && C.opj_set_decode_area(jp2.codec, jp2.image, C.OPJ_INT32(r.Min.X), C.OPJ_INT32(r.Min.Y), C.OPJ_INT32(r.Max.X), C.OPJ_INT32(r.Max.Y)) == C.OPJ_FALSE {
-		err = errors.New("failed to set the decoded area")
+	goLog(6, fmt.Sprintf("num comps: %d", jp2.image.numcomps))
+	goLog(6, fmt.Sprintf("x0: %d, x1: %d, y0: %d, y1: %d", jp2.image.x0, jp2.image.x1, jp2.image.y0, jp2.image.y1))
+
+	if C.opj_set_decode_area(jp2.codec, jp2.image, C.OPJ_INT32(r.Min.X), C.OPJ_INT32(r.Min.Y), C.OPJ_INT32(r.Max.X), C.OPJ_INT32(r.Max.Y)) == C.OPJ_FALSE {
+		return nil, errors.New("failed to set the decoded area")
 	}
 
-	if err == nil && C.opj_decode(jp2.codec, jp2.stream, jp2.image) == C.OPJ_FALSE {
-		err = errors.New("failed to decode image")
+	if C.opj_decode(jp2.codec, jp2.stream, jp2.image) == C.OPJ_FALSE {
+		return nil, errors.New("failed to decode image")
 	}
-	if err == nil && C.opj_end_decompress(jp2.codec, jp2.stream) == C.OPJ_FALSE {
-		err = errors.New("failed to decode image")
+	if C.opj_end_decompress(jp2.codec, jp2.stream) == C.OPJ_FALSE {
+		return nil, errors.New("failed to decode image")
 	}
 
-	if err == nil {
-		var comps []C.opj_image_comp_t
-		compsSlice := (*reflect.SliceHeader)((unsafe.Pointer(&comps)))
-		compsSlice.Cap = int(jp2.image.numcomps)
-		compsSlice.Len = int(jp2.image.numcomps)
-		compsSlice.Data = uintptr(unsafe.Pointer(jp2.image.comps))
+	var comps []C.opj_image_comp_t
+	compsSlice := (*reflect.SliceHeader)((unsafe.Pointer(&comps)))
+	compsSlice.Cap = int(jp2.image.numcomps)
+	compsSlice.Len = int(jp2.image.numcomps)
+	compsSlice.Data = uintptr(unsafe.Pointer(jp2.image.comps))
 
-		bounds := image.Rect(0, 0, int(comps[0].w), int(comps[0].h))
+	bounds := image.Rect(0, 0, int(comps[0].w), int(comps[0].h))
 
-		var data []int32
-		dataSlice := (*reflect.SliceHeader)((unsafe.Pointer(&data)))
-		dataSlice.Cap = bounds.Dx() * bounds.Dy()
-		dataSlice.Len = bounds.Dx() * bounds.Dy()
-		dataSlice.Data = uintptr(unsafe.Pointer(comps[0].data))
+	var data []int32
+	dataSlice := (*reflect.SliceHeader)((unsafe.Pointer(&data)))
+	dataSlice.Cap = bounds.Dx() * bounds.Dy()
+	dataSlice.Len = bounds.Dx() * bounds.Dy()
+	dataSlice.Data = uintptr(unsafe.Pointer(comps[0].data))
 
-		tile = &ImageTile{data, bounds, bounds.Dx(), jp2.image}
-	}
-	return
+	return &ImageTile{data, bounds, bounds.Dx(), jp2.image}, nil
 }
 
 type ImageTile struct {
