3c0ebd0624d3e8fa1c8f0b28eb19cf64e0fbb725
Jeremy Echols <jechols@uoregon.edu>
2014-09-19 15:35:15 -0700

    Add thumbnail handler

--- cmd/jp2tileserver/main.go
+++ cmd/jp2tileserver/main.go
@@ -13,10 +13,12 @@ import (
 	"os"
 	"time"
 
+	"github.com/nfnt/resize"
 	"github.com/uoregon-libraries/newspaper-jp2-viewer/openjpeg"
 )
 
 var tilePathRegex = regexp.MustCompile(`/images/tiles/(?P<path>.+)/image_(?P<width>\d+)x(?P<height>\d+)_from_(?P<x1>\d+),(?P<y1>\d+)_to_(?P<x2>\d+),(?P<y2>\d+).jpg`)
+var thumbPathRegex = regexp.MustCompile(`/images/thumb/(.+)/(\d+)x(\d+)`)
 var infoPathRegex = regexp.MustCompile(`^/images/info/(.+)$`)
 
 var tilePath string
@@ -106,6 +108,48 @@ func TileHandler(w http.ResponseWriter, req *http.Request) {
 	}
 }
 
+func ThumbHandler(w http.ResponseWriter, req *http.Request) {
+	// Extract request path's regex parts into local variables
+	parts := thumbPathRegex.FindStringSubmatch(req.URL.Path)
+	if parts == nil {
+		http.Error(w, "Invalid thumbnail request", 400)
+		return
+	}
+
+	path := parts[1]
+	width, _ := strconv.Atoi(parts[2])
+	height, _ := strconv.Atoi(parts[3])
+
+	// Get file's last modified time, returning a 404 if we can't stat the file
+	filepath := tilePath + "/" + path
+
+	if err := sendCacheHeaders(w, filepath); err != nil {
+		http.Error(w, "Unable to access file " + path, 404)
+		return
+	}
+
+	// Create JP2 structure
+	jp2 := openjpeg.NewJP2Image(filepath)
+	defer jp2.CleanupResources()
+
+	// Pull raw tile data
+	jp2.SetResize(width, height)
+	img, err := jp2.RawImage()
+	if err != nil {
+		http.Error(w, "Unable to read source image", 500)
+		log.Println("Unable to read source image: ", err)
+		return
+	}
+
+	// Encode as JPEG straight to the client
+	resized := resize.Resize(uint(width), uint(height), img, resize.Bilinear)
+	if err = jpeg.Encode(w, resized, &jpeg.Options{Quality: 80}); err != nil {
+		http.Error(w, "Unable to encode tile", 500)
+		log.Println("Unable to encode tile into JPEG:", err)
+		return
+	}
+}
+
 func main() {
 	runtime.GOMAXPROCS(runtime.NumCPU())
 
@@ -127,6 +171,7 @@ func main() {
 
 	http.HandleFunc("/images/tiles/", TileHandler)
 	http.HandleFunc("/images/info/", InfoHandler)
+	http.HandleFunc("/images/thumb/", ThumbHandler)
 
 	if err := http.ListenAndServe(address, nil); err != nil {
 		log.Print("ListenAndServe:", err)
