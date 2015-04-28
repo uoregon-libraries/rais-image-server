b39c937b763c9cb953f6b216c2787319c7b8eea3
Jeremy Echols <jechols@uoregon.edu>
2015-04-28 10:47:44 -0700

    Simplify test functions for iiif_region_test
    - Adds "assert" for simple boolean testing
    - Switches "assertEqualF" to simply typeless "assertEqual"

--- cmd/jp2tileserver/iiif_region_test.go
+++ cmd/jp2tileserver/iiif_region_test.go
@@ -4,41 +4,41 @@ import (
 	"testing"
 )
 
-func assertEqualF(expected, actual float64, message string, t *testing.T) {
-	if expected != actual {
-		t.Errorf("Expected %g, but got %g - %s", expected, actual, message)
+func assert(expression bool, message string, t *testing.T) {
+	if !expression {
+		t.Errorf(message)
 		return
 	}
 	t.Log(message)
 }
 
-func TestPercent(t *testing.T) {
+func assertEqual(expected, actual interface{}, message string, t *testing.T) {
+	if expected != actual {
+		t.Errorf("Expected %#v, but got %#v - %s", expected, actual, message)
+		return
+	}
+	t.Log(message)
+}
+
+func TestRegionTypePercent(t *testing.T) {
 	r := StringToRegion("pct:41.6,7.5,40,70")
-	if r.Type != RTPercent {
-		t.Errorf("Expected r.Type to be RTPercent")
-	}
-
-	assertEqualF(41.6, r.X, "r.X", t)
-	assertEqualF(7.5, r.Y, "r.Y", t)
-	assertEqualF(40, r.W, "r.W", t)
-	assertEqualF(70, r.H, "r.H", t)
+	assert(r.Type == RTPercent, "r.Type == RTPercent", t)
+	assertEqual(41.6, r.X, "r.X", t)
+	assertEqual(7.5, r.Y, "r.Y", t)
+	assertEqual(40.0, r.W, "r.W", t)
+	assertEqual(70.0, r.H, "r.H", t)
 }
 
-func TestPixels(t *testing.T) {
+func TestRegionTypePixels(t *testing.T) {
 	r := StringToRegion("10,10,40,70")
-	if r.Type != RTPixel {
-		t.Errorf("Expected r.Type to be RTPixel")
-	}
-
-	assertEqualF(10, r.X, "r.X", t)
-	assertEqualF(10, r.Y, "r.Y", t)
-	assertEqualF(40, r.W, "r.W", t)
-	assertEqualF(70, r.H, "r.H", t)
+	assert(r.Type == RTPixel, "r.Type == RTPixel", t)
+	assertEqual(10.0, r.X, "r.X", t)
+	assertEqual(10.0, r.Y, "r.Y", t)
+	assertEqual(40.0, r.W, "r.W", t)
+	assertEqual(70.0, r.H, "r.H", t)
 }
 
-func TestFull(t *testing.T) {
+func TestRegionTypeFull(t *testing.T) {
 	r := StringToRegion("full")
-	if r.Type != RTFull {
-		t.Errorf("Expected r.Type to be RTFull")
-	}
+	assert(r.Type == RTFull, "r.Type == RTFull", t)
 }
