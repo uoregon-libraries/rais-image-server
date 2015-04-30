af28527897b87d06a04efb097b829a90b6508d45
Jeremy Echols <jechols@uoregon.edu>
2015-04-30 14:27:38 -0700

    Add initial support for IIIF command paths

--- cmd/jp2tileserver/iiif_handler.go
+++ cmd/jp2tileserver/iiif_handler.go
@@ -5,6 +5,8 @@ import (
 	"fmt"
 	"github.com/uoregon-libraries/newspaper-jp2-viewer/iiif"
 	"github.com/uoregon-libraries/newspaper-jp2-viewer/openjpeg"
+	"image"
+	"image/jpeg"
 	"log"
 	"net/http"
 	"os"
@@ -24,7 +26,7 @@ func IIIFHandler(w http.ResponseWriter, req *http.Request) {
 
 	// No info path should mean a full command path
 	if u := iiif.NewURL(p); u.Valid() {
-		//iiifCommandHandler(w, req, u)
+		iiifCommandHandler(w, req, u)
 		return
 	}
 
@@ -70,4 +72,59 @@ func iiifInfoHandler(w http.ResponseWriter, req *http.Request, id string) {
 	// Set headers - TODO: check for Accept header with jsonld content type!
 	w.Header().Set("Content-Type", "application/json")
 	w.Write(json)
+}
+
+// Handles crop/resize operations for JP2s.  Note that this is the *wrong* way
+// to handle most image formats.  JP2s are encoded as multi-resolution images,
+// so the resize information actually has to be known before a given region can
+// be cropped.  Otherwise we'd have to decode the whole image instead of just
+// the minimum necessary "layer".
+func iiifCommandHandler(w http.ResponseWriter, req *http.Request, u *iiif.URL) {
+	// Get file's last modified time, returning a 404 if we can't stat the file
+	filepath := tilePath + "/" + u.ID
+	if err := sendHeaders(w, req, filepath); err != nil {
+		return
+	}
+
+	// Do we support this request?  If not, return a 501
+	if !iiif.FeaturesLevel1.Supported(u) {
+		http.Error(w, "Feature not supported", 501)
+		return
+	}
+
+	// Create JP2 structure - if we can't, the image must be corrupt or otherwise
+	// broken, since we already checked for existence
+	jp2, err := openjpeg.NewJP2Image(filepath)
+	if err != nil {
+		http.Error(w, "Unable to read source image", 500)
+		log.Println("Unable to read source image: ", err)
+		return
+	}
+	defer jp2.CleanupResources()
+
+	if u.Region.Type == iiif.RTPixel {
+		r := image.Rect(
+			int(u.Region.X),
+			int(u.Region.Y),
+			int(u.Region.X + u.Region.W),
+			int(u.Region.Y + u.Region.H),
+		)
+		jp2.SetCrop(r)
+	}
+
+	// TODO: convert size to w/h
+	//jp2.SetResize(width, height)
+	img, err := jp2.DecodeImage()
+	if err != nil {
+		http.Error(w, "Unable to decode image", 500)
+		log.Println("Unable to decode image: ", err)
+		return
+	}
+
+	// Encode as JPEG straight to the client
+	if err = jpeg.Encode(w, img, &jpeg.Options{Quality: 80}); err != nil {
+		http.Error(w, "Unable to encode jpeg", 500)
+		log.Println("Unable to encode JPEG:", err)
+		return
+	}
 }
