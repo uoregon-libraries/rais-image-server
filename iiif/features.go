ec4ce7db03890a59b018f2261d327ceacc3e4069
Jeremy Echols <jechols@uoregon.edu>
2015-05-11 15:51:07 -0700

    Move info-related code from features.go to info.go

--- iiif/features.go
+++ iiif/features.go
@@ -65,42 +65,6 @@ type FeatureSet struct {
 	TileSizes []TileSize
 }
 
-// Info returns the default structure for a FeatureSet's info response JSON.
-// The caller is responsible for filling in image-specific values (ID and
-// dimensions).
-func (fs *FeatureSet) Info() *Info {
-	i := NewInfo()
-	i.Profile = fs.Profile()
-	i.Tiles = fs.TileSizes
-
-	return i
-}
-
-// Profile examines the features in the FeatureSet to determine first which
-// level the FeatureSet supports, then adds any variances.
-func (fs *FeatureSet) Profile() []profile {
-	p := make([]profile, 1)
-
-	var baseFeatureSet *FeatureSet
-	if fs.includes(FeaturesLevel2) {
-		baseFeatureSet = FeaturesLevel2
-		p[0] = profile("http://iiif.io/api/image/2/level2.json")
-	} else if fs.includes(FeaturesLevel1) {
-		baseFeatureSet = FeaturesLevel1
-		p[0] = profile("http://iiif.io/api/image/2/level1.json")
-	} else {
-		baseFeatureSet = FeaturesLevel0
-		p[0] = profile("http://iiif.io/api/image/2/level0.json")
-	}
-
-	_, extraFeatures, _ := FeatureCompare(fs, baseFeatureSet)
-
-	if len(extraFeatures) > 0 {
-	}
-
-	return p
-}
-
 // toMap converts a FeatureSet's boolean support values into a map suitable for
 // use in comparison to other feature sets.  The strings used are lowercased so
 // they can be used as-is within "formats", "qualities", and/or "supports"
--- iiif/info.go
+++ iiif/info.go
@@ -21,3 +21,39 @@ func NewInfo() *Info {
 		Protocol: "http://iiif.io/api/image",
 	}
 }
+
+// Info returns the default structure for a FeatureSet's info response JSON.
+// The caller is responsible for filling in image-specific values (ID and
+// dimensions).
+func (fs *FeatureSet) Info() *Info {
+	i := NewInfo()
+	i.Profile = fs.Profile()
+	i.Tiles = fs.TileSizes
+
+	return i
+}
+
+// Profile examines the features in the FeatureSet to determine first which
+// level the FeatureSet supports, then adds any variances.
+func (fs *FeatureSet) Profile() []profile {
+	p := make([]profile, 1)
+
+	var baseFeatureSet *FeatureSet
+	if fs.includes(FeaturesLevel2) {
+		baseFeatureSet = FeaturesLevel2
+		p[0] = profile("http://iiif.io/api/image/2/level2.json")
+	} else if fs.includes(FeaturesLevel1) {
+		baseFeatureSet = FeaturesLevel1
+		p[0] = profile("http://iiif.io/api/image/2/level1.json")
+	} else {
+		baseFeatureSet = FeaturesLevel0
+		p[0] = profile("http://iiif.io/api/image/2/level0.json")
+	}
+
+	_, extraFeatures, _ := FeatureCompare(fs, baseFeatureSet)
+
+	if len(extraFeatures) > 0 {
+	}
+
+	return p
+}
