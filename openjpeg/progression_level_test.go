a9446267a054ad49ee4c9621f02beec7ccec92dc
Jeremy Echols <jechols@uoregon.edu>
2015-05-17 11:35:10 -0700

    Switch to color-assert for assertEqualInt tests

--- openjpeg/progression_level_test.go
+++ openjpeg/progression_level_test.go
@@ -1,17 +1,10 @@
 package openjpeg
 
 import (
+	"github.com/uoregon-libraries/rais-image-server/color-assert"
 	"image"
 	"testing"
 )
-
-func assertEqualInt(expected, actual int, message string, t *testing.T) {
-	if expected != actual {
-		t.Errorf("Expected %d, but got %d - %s", expected, actual, message)
-		return
-	}
-	t.Log(message)
-}
 
 func TestDesiredProgressionLevel(t *testing.T) {
 	source := image.Rect(0, 0, 5000, 5000)
@@ -19,11 +12,11 @@ func TestDesiredProgressionLevel(t *testing.T) {
 		return desiredProgressionLevel(source, w, h)
 	}
 
-	assertEqualInt(0, dpl(5000, 5000), "Source and dest are equal, level should be 0", t)
-	assertEqualInt(0, dpl(5001, 5001), "Source is SMALLER than dest, so level has to be 0", t)
-	assertEqualInt(0, dpl(4999, 4999), "Source is larger than dest, but not by a factor of 2, so level has to be 0", t)
-	assertEqualInt(0, dpl(2501, 2501), "Source is just under 2x dest, so level still has to be 0", t)
-	assertEqualInt(1, dpl(2500, 2500), "Source is exactly 2x dest, so level has to be 1", t)
-	assertEqualInt(1, dpl(2500, 250), "We have to pick the largest dimension, so level for 2500x250 should be 1", t)
-	assertEqualInt(2, dpl(1250, 0), "Use the non-zero dimension for resize package compatibility", t)
+	assert.Equal(0, dpl(5000, 5000), "Source and dest are equal, level should be 0", t)
+	assert.Equal(0, dpl(5001, 5001), "Source is SMALLER than dest, so level has to be 0", t)
+	assert.Equal(0, dpl(4999, 4999), "Source is larger than dest, but not by a factor of 2, so level has to be 0", t)
+	assert.Equal(0, dpl(2501, 2501), "Source is just under 2x dest, so level still has to be 0", t)
+	assert.Equal(1, dpl(2500, 2500), "Source is exactly 2x dest, so level has to be 1", t)
+	assert.Equal(1, dpl(2500, 250), "We have to pick the largest dimension, so level for 2500x250 should be 1", t)
+	assert.Equal(2, dpl(1250, 0), "Use the non-zero dimension for resize package compatibility", t)
 }
