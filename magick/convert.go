a11467e49b8d8d8f31c0f7f50d13f02675f41ff1
Jeremy Echols <jechols@uoregon.edu>
2015-06-19 11:26:51 -0700

    Convert imagick image to go image

--- magick/convert.go
+++ magick/convert.go
@@ -8,9 +8,38 @@ package magick
 import "C"
 import (
 	"image"
+	"reflect"
+	"unsafe"
 )
 
-// Image returns a native Go image interface
+// This string never changes, and is used for the life of the app, so it's safe
+// not to free
+var cRGBA = C.CString("RGBA")
+var ssizeZero = C.ssize_t(0)
+
+// Image returns a native Go image interface.  For now, this is always RGBA for
+// simplicity, but it would be a good idea to use a gray image when it makes
+// sense to improve performance and RAM usage.
 func (i *Image) Image() (image.Image, error) {
-	return image.NewRGBA(image.Rect(0, 0, i.decodeWidth, i.decodeHeight)), nil
+	// Create and prep-for-freeing the exception
+	exception := C.AcquireExceptionInfo()
+	defer C.DestroyExceptionInfo(exception)
+
+	img := image.NewRGBA(image.Rect(0, 0, i.decodeWidth, i.decodeHeight))
+
+	area := i.decodeWidth * i.decodeHeight
+	pixLen := area << 2
+	pixels := make([]byte, pixLen)
+	pi := reflect.ValueOf(pixels).Interface()
+	ptr := unsafe.Pointer(&pixels[0])
+
+	// Dimensions as C types
+	w := C.size_t(i.decodeWidth)
+	h := C.size_t(i.decodeHeight)
+
+	C.ExportImagePixels(i.image, ssizeZero, ssizeZero, w, h, cRGBA, C.CharPixel, ptr, exception)
+
+	img.Pix = pi.([]uint8)
+
+	return img, nil
 }
