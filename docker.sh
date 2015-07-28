76cc0dea6c3c68f1a78a1eebe3161389cf770c44
Jeremy Echols <jechols@uoregon.edu>
2015-07-28 09:34:23 -0700

    Add example shell script for building container

---
+++ docker.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+#
+# docker.sh: Removes and rebuilds the rais container with an example of how to
+# run it with various settings.
+#
+# After starting the container, test RAIS by browsing to
+# http://localhost:12415/iiif/test-world.jp2/full/full/0/default.jpg
+
+docker stop rais
+docker rm rais
+docker build -t rais:master .
+
+# All possible environmental overrides are included below for clarity:
+# - PORT: the port RAIS listens on, defaults to 12415
+# - TILESIZES: what RAIS reports as valid IIIF tile sizes, defaults to 512
+# - IIIFURL: what RAIS reports as its server URL, defaults to localhost:$PORT/iiif
+docker run -d \
+  --name rais \
+  -e PORT=12415 \
+  -e TILESIZES=512 \
+  -e IIIFURL="http://localhost:12415/iiif" \
+  -p 12415:12415 \
+  -v $(pwd)/testfile:/var/local/images \
+  rais:master
