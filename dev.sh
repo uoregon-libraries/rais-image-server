6679fb81e3224c8422b58d0c44b134c94da298c7
Jeremy Echols <jechols@uoregon.edu>
2016-04-14 14:34:20 -0700

    Revert "undo me"
    This reverts commit cee1a5176eddf8d31e736b9649a88f97cef537e1.
    
    Oops.

--- dev.sh
+++ dev.sh
@@ -7,7 +7,6 @@ docker run -it --rm \
   --name rais \
   --privileged=true \
   -p 12415:12415 \
-  -v /mnt/news2/outgoing/batch_oru_20160329203731_ver01/data/sn00063676/print/2015080701:/var/local/images/tmp \
   -v $(pwd):/opt/rais-src \
   rais-build /opt/rais-src/bin/rais-server --address ":12415" \
       --iiif-url "http://localhost:12415/iiif" \
