ebc701496a6778b9ccc9a862bed948a13049f58a
Jeremy Echols <jechols@uoregon.edu>
2016-05-05 15:02:21 -0700

    Add jp2info utility and package
    Bits and pieces of algorithms have been lovingly sewn together to
    produce the "jp2info" package, which should allow us to actually pull in
    real data about a JP2 file's number of levels.
    
    - Adds a package, jp2info, which pulls some basic information from a JP2
      file into a nice structure
    - Adds a binary command, `bin/jp2info`, which can be run against JP2
      files to get an idea of some info which can be quickly reported
    
    This doesn't do anywhere near all JP2 info, or even info on all JP2
    files.  For now, it's enough for RAIS and a little extra.

---
+++ src/cmd/jp2info/main.go
@@ -0,0 +1,53 @@
+package main
+
+import (
+	"fmt"
+	"jp2info"
+	"os"
+
+	"github.com/jessevdk/go-flags"
+)
+
+var opts struct {
+	Raw bool `short:"r" long:"raw" description:"show raw JP2 info structure"`
+}
+
+func main() {
+	var args []string
+	var err error
+
+	var parser = flags.NewParser(&opts, flags.Default)
+	parser.Usage = "filename [filename...] [OPTIONS]"
+	args, err = parser.Parse()
+
+	if err != nil || len(args) < 1 {
+		parser.WriteHelp(os.Stderr)
+		os.Exit(1)
+	}
+
+	var arg string
+	var s = jp2info.NewScanner()
+	for _, arg = range args {
+		fmt.Printf("%s: ", arg)
+		printScanResults(s.Scan(arg))
+	}
+}
+
+func printScanResults(i *jp2info.Info, err error) {
+	if err != nil {
+		// Invalid file or some variation of the spec that isn't supported
+		fmt.Printf("Error: %s\n", err)
+		return
+	}
+
+	if opts.Raw {
+		fmt.Printf("%#v\n", i)
+	} else {
+		printInfo(i)
+	}
+}
+
+func printInfo(i *jp2info.Info) {
+	fmt.Printf("dim:%dx%d tiles:%dx%d levels:%d %s\n",
+		i.Width, i.Height, i.TileWidth(), i.TileHeight(), i.Levels, i.ColorSpace.String())
+}
