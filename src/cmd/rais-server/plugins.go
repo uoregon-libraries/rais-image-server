9c62f9aab1de9c4509f606cc2cad0521e6d4a265
Jeremy Echols <jechols@uoregon.edu>
2018-10-05 14:48:39 +0000

    rais-server: refactor plugin-loading logic
    Only runs / indexes plugins' functions after a plugin has been
    verified.  If a single exposed function is invalid, we no longer
    consider the plugin safe to use in any way.
    
    All type assertions are now down via the plug* type rather than a raw
    function (not sure why this wasn't always the case)

--- src/cmd/rais-server/plugins.go
+++ src/cmd/rais-server/plugins.go
@@ -11,11 +11,13 @@ import (
 	"github.com/uoregon-libraries/gopkg/logger"
 )
 
+type plugGeneric func()
 type plugIDToPath func(iiif.ID) (string, error)
+type plugLogger func(*logger.Logger)
 
 var idToPathPlugins []plugIDToPath
 
-// LoadPlugins searches for any plugins in the current working directory /plugins
+// LoadPlugins searches for any plugins in the binary's directory + /plugins
 func LoadPlugins(l *logger.Logger) {
 	var dir = filepath.Dir(os.Args[0])
 	var plugdir = filepath.Join(dir, "plugins")
@@ -53,6 +55,14 @@ func LoadPlugins(l *logger.Logger) {
 	}
 }
 
+// loadPlugin attempts to read the given plugin file and extract known symbols.
+// If a plugin exposes Initialize or SetLogger, they're called here once we're
+// sure the plugin is valid.  IDToPath functions are indexed globally for use
+// in the RAIS image serving handler.
+//
+// This function is unnecessarily complicated and needs refactoring.  Other
+// than the concrete type, the "index a function" blocks are all basically the
+// same.
 func loadPlugin(fullpath string, l *logger.Logger) {
 	var p, err = plugin.Open(fullpath)
 	if err != nil {
@@ -61,37 +71,62 @@ func loadPlugin(fullpath string, l *logger.Logger) {
 	}
 
 	var sym plugin.Symbol
+	var fnCount int
 
+	var log plugLogger
 	sym, err = p.Lookup("SetLogger")
 	if err == nil {
-		var setLogger, ok = sym.(func(*logger.Logger))
-		if ok {
-			l.Debugf("Attaching central logger to plugin")
-			setLogger(l)
+		var f, ok = sym.(plugLogger)
+		if !ok {
+			l.Errorf("%q.SetLogger is invalid", fullpath)
+			return
 		}
+		l.Debugf("Found %q.SetLogger", fullpath)
+		log = plugLogger(f)
+		fnCount++
 	}
 
+	var idToPath plugIDToPath
 	sym, err = p.Lookup("IDToPath")
 	if err == nil {
-		var f, ok = sym.(func(iiif.ID) (string, error))
+		var f, ok = sym.(plugIDToPath)
 		if !ok {
-			l.Errorf("Plugin %q exposes an invalid IDToPath function", fullpath)
+			l.Errorf("%q.IDToPath is invalid", fullpath)
 			return
 		}
-
-		l.Debugf("Adding IDToPath from %q", fullpath)
-		idToPathPlugins = append(idToPathPlugins, plugIDToPath(f))
+		l.Debugf("Found %q.IDToPath", fullpath)
+		idToPath = plugIDToPath(f)
+		fnCount++
 	}
 
+	var init plugGeneric
 	sym, err = p.Lookup("Initialize")
 	if err == nil {
-		var f, ok = sym.(func())
+		var f, ok = sym.(plugGeneric)
 		if !ok {
-			l.Errorf("Plugin %q exposes an invalid Initialize function", fullpath)
+			l.Errorf("%q.Initialize is invalid", fullpath)
 			return
 		}
+		l.Debugf("Found %q.Initialize", fullpath)
+		init = plugGeneric(f)
+		fnCount++
+	}
 
-		l.Debugf("Calling %q.Initialize()", fullpath)
-		f()
+	if fnCount == 0 {
+		l.Warnf("%q doesn't expose any known functions", fullpath)
+		return
+	}
+
+	// We can call SetLogger and Initialize immediately, as they're never called a second time
+	if log != nil {
+		log(l)
+	}
+	if init != nil {
+		init()
+	}
+
+	// Index the IDToPath function if available
+	if idToPath != nil {
+		idToPathPlugins = append(idToPathPlugins, idToPath)
 	}
 }
