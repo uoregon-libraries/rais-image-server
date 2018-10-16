690bbeab2f755bf8f5c6853ee73b4d4faf976811
Jeremy Echols <jechols@uoregon.edu>
2018-10-16 13:05:16 -0700

    openjpeg: get rid of excessive logging
    Per-request logging was removed, and openjpeg's "info" logging has been
    disabled entirely for now - it's entirely too verbose even for a "debug"
    mode

--- src/openjpeg/logging.go
+++ src/openjpeg/logging.go
@@ -14,14 +14,6 @@ import (
 // can be overridden (as is the case with the main RAIS command)
 var Logger = logger.DefaultLogger
 
-// GoLogInfo bridges the openjpeg logging with our internal logger.  We map
-// openjpeg's "info" logging to debug because these tend to be very verbose and
-// not terribly important.
-//export GoLogInfo
-func GoLogInfo(cmessage *C.char) {
-	log(Logger.Debugf, cmessage)
-}
-
 // GoLogWarning bridges the openjpeg logging with our internal logger
 //export GoLogWarning
 func GoLogWarning(cmessage *C.char) {
