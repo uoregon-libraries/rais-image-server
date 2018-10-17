c91b5175ca42907cb6dbf1f84e1c2ea5b90f2559
Jeremy Echols <jechols@uoregon.edu>
2018-10-17 15:37:06 -0700

    plugins/json-tracer: infer request type
    Adds a new field, "Type", to the traced data, which will allow better
    filtering for things like tile timing, thumbnail timing, etc.

--- src/plugins/json-tracer/tracer.go
+++ src/plugins/json-tracer/tracer.go
@@ -2,12 +2,15 @@ package main
 
 import (
 	"net/http"
+	"rais/src/iiif"
+	"strings"
 	"sync"
 	"time"
 )
 
 type trace struct {
 	Path     string
+	Type     string
 	Start    time.Time
 	Duration float64
 	Status   int
@@ -55,12 +58,50 @@ func (t *tracer) ServeHTTP(w http.ResponseWriter, req *http.Request) {
 	go t.appendTrace(path, now, time.Since(now), sr.status)
 }
 
+// getReqType is a bit ugly and hacky, but attempts to determine what kind of
+// IIIF request we have, if any.  Determining type isn't as easy without
+// peeking at RAIS's config, which seems like it could break one day (say the
+// IIIF URL config changes but we forget to update this plugin).  Instead, we
+// just do what we can by pulling the URL apart as necessary to make pretty
+// good guesses.
+func getReqType(path string) string {
+	if len(path) < 9 {
+		return "None"
+	}
+
+	if path[len(path)-9:] == "info.json" {
+		return "Info"
+	}
+
+	var parts = strings.Split(path, "/")
+	if len(parts) < 5 {
+		return "None"
+	}
+
+	var iiifPath = strings.Join(parts[len(parts)-5:], "/")
+	var u, err = iiif.NewURL(iiifPath)
+	if err != nil || !u.Valid() {
+		return "None"
+	}
+
+	if err == nil {
+		if u.Region.Type == iiif.RTFull || u.Region.Type == iiif.RTSquare {
+			return "Resize"
+		} else if u.Size.W <= 1024 && u.Size.H <= 1024 {
+			return "Tile"
+		}
+	}
+
+	return "Unknown"
+}
+
 func (t *tracer) appendTrace(path string, start time.Time, duration time.Duration, status int) {
 	t.Lock()
 	defer t.Unlock()
 
 	t.traceList.list = append(t.traceList.list, trace{
 		Path:     path,
+		Type:     getReqType(path),
 		Start:    start,
 		Duration: duration.Seconds(),
 		Status:   status,
